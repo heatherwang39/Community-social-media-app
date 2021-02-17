ba70e07e2498ff239286ea7e086cb63836747e7d
unknown <heatherwang39@gmail.com>
2021-02-17 15:43:39 -0500

    add basic layout for Caption page, without word count up and hash tag placeholder

---
+++ app/src/main/java/com/example/ece1778/Caption.java
@@ -0,0 +1,14 @@
+package com.example.ece1778;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Caption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caption);
+    }
+}
